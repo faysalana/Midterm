3e251d5d401880fae4df65d4bf3925c862a07357
faysalana <faysalahmad858@gmail.com>
2023-08-19 16:02:28 +0300

    updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import datetime
 
 
 
@@ -29,7 +29,7 @@ def login(d):
         password = input('Enter password: ')
 
         if password == 'admin123123' and username == 'admin':
-            admin_menu()
+            admin_menu(d)
             break
 
         elif username in d and password == "" :
@@ -50,16 +50,55 @@ def a_menu():
     print("6. Raise Employee's Salary")
     print("7. Exit")
     pass
-def admin_menu():
+
+
+def add_employee(d):
+    emp_id = "emp" + str(len(d) + 1)
+    username = input("Enter username: ")
+    gender = input("Enter gender (male/female): ")
+    salary = int(input("Enter salary: "))
+    timestamp = datetime.datetime.now().strftime('%Y%m%d')  # I know this from a Female Automated Voice Assistant that I made before
+
+    d[username] = {
+        'emp_id': emp_id,
+        'Date of Birth': timestamp,  # You can update this with actual DOB input if needed
+        'Gender': gender,
+        'Salary': salary
+    }
+
+    with open("Employees.txt", "a") as f:
+        # f.write(f"\n{emp_id}, {username}, {timestamp}, {gender}, {salary}\n")
+        f.write('{}, {}, {}, {}, {}\n'.format(emp_id, username, timestamp, gender,salary))   #https://stackoverflow.com/questions/6931183/how-to-write-multiple-values-into-one-line-in-a-text-file
+
+    print("Employee added successfully!")
+def admin_menu(d):
     print("welcome admin")
+
     while True:
         a_menu()
         x = input("Choose a number: ")
-        if x=='1':
-            #display
-        elif x=='2':
+        if x == '1':
+            male = sum(1 for employee in d.values() if employee['Gender'] == 'male') # I took this snippet from here https://www.geeksforgeeks.org/python-sum-values-for-each-key-in-nested-dictionary/
+            female = sum(1 for employee in d.values() if employee['Gender'] == 'female')
+            print("Male Employees:", male)
+            print("Female Employees:", female)
 
-        elif
+        elif x == '2':
+            add_employee(d)
+
+        elif x == '3':
+            pass
+        elif x == '4':
+            pass
+        elif x == '5':
+            pass
+        elif x == '6':
+            pass
+        elif x == '7':
+            pass
+        else:
+            print("Wrong input")
+
 
 
 
